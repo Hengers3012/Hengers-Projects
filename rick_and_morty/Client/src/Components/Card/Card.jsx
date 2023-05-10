6c1278f229a165154a25f238aaf5a7c93d7eb8d3
Hengers3012 <hengersrosario.m@gmail.com>
2023-05-10 00:59:13 -0400

    Update 10/05/2023 12:58am Front

--- rick_and_morty/Client/src/Components/Card/Card.jsx
+++ rick_and_morty/Client/src/Components/Card/Card.jsx
@@ -2,20 +2,22 @@ import cardStyles from "./Card.module.css";
 export default function Card({ name, species, image, gender, onClose }) {
   return (
     <div className={cardStyles.containerCard}>
-      <button onClick={onClose}>X</button>
+      <dir className={cardStyles.containerButtonX}>
+        <button onClick={onClose}>❌</button>
+      </dir>
+
+      <div className={cardStyles.imageCharacter}>
+        <img src={image} alt="Not found" />
+      </div>
 
       <div className={cardStyles.nameCharacter}>
         <h2>{name}</h2>
       </div>
 
-      <div>
-        <img src={image} alt="Not found" />
-      </div>
-
-      <div>
+      {/* <div className={cardStyles.descriptionCharacter}>
         <h2>{species}</h2>
         <h2>{gender}</h2>
-      </div>
+      </div> */}
     </div>
   );
 }

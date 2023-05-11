5483df9a4427bbeb592ea96992ec64c21e0293ab
Hengers3012 <hengersrosario.m@gmail.com>
2023-05-11 02:50:35 -0400

    Update 11/05/2023 02:50am Front

--- rick_and_morty/Client/src/Components/Card/Card.jsx
+++ rick_and_morty/Client/src/Components/Card/Card.jsx
@@ -7,14 +7,14 @@ export default function Card({ name, species, image, gender, onClose }) {
           <i class="fa-solid fa-trash"></i>
         </button>
       </div>
-
-      <div className={cardStyles.imageCharacter}>
-        <img src={image} alt="Not found" />
+      <div className={cardStyles.containerInfo}>
+        <div className={cardStyles.imageCharacter}>
+          <img src={image} alt="Not found" />
+        </div>{" "}
         <div className={cardStyles.nameCharacter}>
           <h2>{name}</h2>
         </div>
       </div>
-
       {/* <div className={cardStyles.descriptionCharacter}>
         <h2>{species}</h2>
         <h2>{gender}</h2>

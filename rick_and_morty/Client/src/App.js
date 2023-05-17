a0c355f9f11267d7d6e1d60d7108263e2619e933
Hengers3012 <hengersrosario.m@gmail.com>
2023-05-17 13:24:25 -0400

    Actualizando Interfaz Grafica de la App.js

--- rick_and_morty/Client/src/App.js
+++ rick_and_morty/Client/src/App.js
@@ -10,7 +10,14 @@ import characters from "./data.js";
 function App() {
   return (
     <div className={appStyles.containerApp}>
+      <div className={appStyles.contStart}>
+        <div className={appStyles.stars} id="stars"></div>
+        <div className={appStyles.stars2} id="stars2"></div>
+        <div className={appStyles.stars3} id="stars3"></div>
+      </div>
+
       <div className={appStyles.nave}></div>
+
       <div className={appStyles.navBar}>
         <NavBar />
       </div>

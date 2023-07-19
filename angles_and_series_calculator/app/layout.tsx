247ea13f418740ad9c1d68ea1ae7701079a7dbaa
Hengers3012 <hengersrosario.m@gmail.com>
2023-07-19 14:53:12 -0400

    feat: Create Repo de angle and series calculator

--- angles_and_series_calculator/app/layout.tsx
+++ angles_and_series_calculator/app/layout.tsx
@@ -18,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
